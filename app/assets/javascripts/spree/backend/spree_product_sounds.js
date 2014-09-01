72281bcc26cbfe9d985c576bf13159f26ac53ce7
Brian Wells <spam_brian@me.com>
2014-09-01 00:24:06 -0400

    Move assets into Spree 2.2 locations (see spree/spree#4050)

--- app/assets/javascripts/spree/backend/spree_product_sounds.js
+++ app/assets/javascripts/spree/backend/spree_product_sounds.js
@@ -1,4 +1,4 @@
-//= require admin/spree_backend
+//= require spree/backend
 //= require miniAudioPlayer/jquery.jplayer.min
 //= require miniAudioPlayer/jquery.mb.miniPlayer
 //= require miniAudioPlayer/miniAudioPlayer
--- app/assets/javascripts/spree/frontend/spree_product_sounds.js
+++ app/assets/javascripts/spree/frontend/spree_product_sounds.js
@@ -1,4 +1,4 @@
-//= require store/spree_frontend
+//= require spree/frontend
 //= require miniAudioPlayer/jquery.jplayer.min
 //= require miniAudioPlayer/jquery.mb.miniPlayer
 //= require miniAudioPlayer/miniAudioPlayer
